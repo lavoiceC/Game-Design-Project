fa6c5903fafcc5ac7e4a8b29153a83cf672bea92
maldonadoc2415 <maldonadoc2415@student.elms.edu>
2026-04-08 22:17:40 -0400

    Update: Player Assets, game.tscn, player.gd started.

---
+++ player.gd
@@ -0,0 +1,18 @@
+extends Area2D
+## how fast the ship rotates
+@export var rot_speed:float = 2.6
+## determines how much thrust the ship has
+@export var thrust: Vector2 = Vector2(500, 0)
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
