dd26890c5bc54c389c86963ada77174e67bf26ae
maldonadoc2415 <maldonadoc2415@student.elms.edu>
2026-04-15 22:50:38 -0400

    Update: Moved variables to correct files, Started working on enemy functions to keep color the same after breakup

---
+++ chip.gd
@@ -0,0 +1,25 @@
+extends Area2D
+signal death
+signal dealt_dmg
+signal breakup(size, extents, pos, vel, hit_vel, color_name)
+var skin_frame:int = 0
+var chip_size = {'Lg': 75, 'Med': 50,"Sm": 25}
+var chip_speed = 50
+var chip_bp = {'Lg': 'Med', 'Med': "Sm", "Sm": null} # break up pattern
+var images = {}
+var row_map = {'Lg': 0, 'Med': 1, 'Sm': 2}
+var color_map = {'Green': 0, 'Blue': 1, 'Red': 2, 'Grey': 3}
+var current_size:String = 'Lg'
+var current_color:String = 'Green'
+
+@onready var sprite = $Sprite2D
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var variation = randi() % 4
+	var col_x = (color_map[current_color] * 4) + variation
+	var row_y = row_map[current_size]
+	sprite.frame_coords = Vector2i(col_x, row_y)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- game.gd
+++ game.gd
@@ -1,22 +1,23 @@
 ## May have received some inspiriation via https://github.com/kidscancode/space_rocks/
+## This file has been set to Autoload at the top of every scene,
+## therefore, by appending "game" to a variable listed, it may be called
+## in another file. (e.g. game.chip_size = = cs, enemy1 = cs['Lg'], etc )
 extends Node2D
 # Settings
-@export var game_over = false
-@export var score = 0
-@export var level = 1
-
+var game_over = false
+var score = 0
+var level = 1
+var screensize = Vector2()
 # chip & virus settings
-@export var break_pattern = {'Lg': 'Med', 'Med': "Sm", "Sm": null}
-@export var chip_size = {'Lg': 75, 'Med': 50,"Sm": 25}
-@export var virus_size = {'Lg': 150, 'Med': 75, 'Sm': 25}
-@export var enemy_dmg = 1
-@export var chip_speed = 50
-@export var virus_speed = chip_speed
+var break_pattern = {'Lg': 'Med', 'Med': "Sm", "Sm": null}
+# Will place Virus and Boss variables inside seperate files 
+var virus_size = {'Lg': 150, 'Med': 75, 'Sm': 25}
+var virus_speed = 70
 # Melissa (Boss) settings
-@export var boss_size = {'s1': 100, 's2': 80, 's3': 60, 's4': 40, 's5': 20} 
-@export var boss_pattern = {'s1': 's2', 's2': 's3', 's3': 's4', 's4': 's5', 's5': null}
-@export var boss_damage = {'s1': 3, 's2': 3, 's3': 2, 's4': 1, 's5': 1}
-@export var boss_speed = {'s1': 0, 's2': 20, 's3': 50, 's4': 75, 's5': 100}
+var boss_size = {'s1': 100, 's2': 80, 's3': 60, 's4': 40, 's5': 20} 
+var boss_pattern = {'s1': 's2', 's2': 's3', 's3': 's4', 's4': 's5', 's5': null}
+var boss_damage = {'s1': 3, 's2': 3, 's3': 2, 's4': 1, 's5': 1}
+var boss_speed = {'s1': 0, 's2': 20, 's3': 50, 's4': 75, 's5': 100}
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
