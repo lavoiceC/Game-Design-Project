9aa6872d7b8221f49af516cbdb838a537b3d9afc
maldonadoc2415 <maldonadoc2415@student.elms.edu>
2026-04-17 14:35:10 -0400

    TRYING TO FIX THIS MESS!

--- game.gd
+++ game.gd
@@ -19,8 +19,9 @@ var boss_pattern = {'s1': 's2', 's2': 's3', 's3': 's4', 's4': 's5', 's5': null}
 var boss_damage = {'s1': 3, 's2': 3, 's3': 2, 's4': 1, 's5': 1}
 var boss_speed = {'s1': 0, 's2': 20, 's3': 50, 's4': 75, 's5': 100}
 # Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
-	$Player.position = Vector2(576,600)
+	$Player.position = Vector2(500,500)
 	
 
 
